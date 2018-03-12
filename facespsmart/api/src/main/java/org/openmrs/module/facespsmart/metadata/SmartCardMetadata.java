0a77fd11aa97327043838d4d9617348eabebe6ac
rugute <rrugute@gmail.com>
2018-03-12 16:14:38 +0300

    Intergration with middleware PatientSHR from the EMR and SHRProcessor generation

--- facespsmart/api/src/main/java/org/openmrs/module/facespsmart/metadata/SmartCardMetadata.java
+++ facespsmart/api/src/main/java/org/openmrs/module/facespsmart/metadata/SmartCardMetadata.java
@@ -1,21 +1,24 @@
 package org.openmrs.module.facespsmart.metadata;
 
+import org.openmrs.PatientIdentifierType;
+import org.openmrs.module.metadatadeploy.bundle.AbstractMetadataBundle;
 import org.springframework.stereotype.Component;
+
+import static org.openmrs.module.metadatadeploy.bundle.CoreConstructors.patientIdentifierType;
 
 
 /**
  * Created by rugute on 2/28/18.
  */
 @Component
-public class SmartCardMetadata
-{
-   /* extends
- AbstractMetadataBundle {
+public class SmartCardMetadata extends AbstractMetadataBundle {
 
     public static final String MODULE_ID = "facespsmart";
 
     public static final class _PatientIdentifierType {
         public static final String SMART_CARD_SERIAL_NUMBER = "8f842498-1c5b-11e8-accf-0ed5f89f718b";
+        public static final String HTS_NUMBER = "e6af3782-1cb3-11e8-accf-0ed5f89f718b";
+        public static final String GODS_NUMBER = "9aedb9ae-1cbd-11e8-accf-0ed5f89f718b";
     }
 
     @Override
@@ -23,5 +26,12 @@ public class SmartCardMetadata
         install(patientIdentifierType("Smart Card Serial Number", "P-SMART Serial Number", null, null,
                 null, PatientIdentifierType.LocationBehavior.NOT_USED, false, _PatientIdentifierType.SMART_CARD_SERIAL_NUMBER));
 
-    }*/
+        install(patientIdentifierType("HTS Number", "Number assigned to clients when tested for HIV", null, null,
+                null, PatientIdentifierType.LocationBehavior.NOT_USED, false, _PatientIdentifierType.HTS_NUMBER));
+
+        install(patientIdentifierType("GODS Number", "Number assigned by MPI", null, null,
+                null, PatientIdentifierType.LocationBehavior.NOT_USED, false, _PatientIdentifierType.GODS_NUMBER));
+
+
+    }
 }
